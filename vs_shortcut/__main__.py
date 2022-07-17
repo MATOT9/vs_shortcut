4cad644214f22c48aeb3d672b1ef47caa4d2033e
Maxime Tousignant=Tremblay <>
2022-07-16 21:33:27 -0400

    Minor __doc__ modifications

--- vs_shortcut/__main__.py
+++ vs_shortcut/__main__.py
@@ -1,10 +1,12 @@
-"""Entry point for the launching script of Microsoft Visual Studio.
+"""
+    Entry point for the launching script of Microsoft Visual Studio.
 
 """
 
 __author__ = "Maxime Tousignant-Tremblay"
 __copyright__ = "Copyright (C) 2022 Maxime Tousignant-Tremblay"
 __license__ = "AGPL-3.0"
+__status__ = "Prototype"
 
 # Standard library
 import os
